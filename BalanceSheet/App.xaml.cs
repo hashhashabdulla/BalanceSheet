832ac6da3ffbd992323d9091f42259d601debe65
hashhash.abdulla <hashhashabdullah@gmail.com>
2020-03-25 11:18:24 +0530

    Add project files.

---
+++ BalanceSheet/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BalanceSheet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public ResourceDictionary ThemeDictionary
+        {
+            // You could probably get it via its name with some query logic as well.
+            get { return Resources.MergedDictionaries[0]; }
+        }
+
+        public void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary.Source = uri;
+        }
+    }
+}
---
+++ BalanceSheet/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using BalanceSheet.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BalanceSheet.ViewModel
+{
+    class MainWindowViewModel
+    {
+        public bool DarkThemeEnabled { get; set; }
+        public ICommand ThemeToggleCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ThemeToggleCommand = new RelayCommand<object>(p => this.OnThemeToggle());
+        }
+
+        private void OnThemeToggle()
+        {
+            if (DarkThemeEnabled)
+            {
+                var app = (App)Application.Current;
+                Uri uri = new Uri("pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Dark.xaml");
+                app.ChangeTheme(uri); 
+            }
+            else
+            {
+                var app = (App)Application.Current;
+                Uri uri = new Uri("pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml");
+                app.ChangeTheme(uri);
+            }
+        }
+    }
+}
